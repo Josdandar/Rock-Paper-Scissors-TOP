8b0c54f79b5e9a13cb104506b8268731ae827ba6
Josdanda <josdandar@gmail.com>
2022-07-04 16:52:52 -0700

    Input case insensitive

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let humanSelection = prompt("Please select rock, paper or scissors");
 
 
-function computerPlay(selection){
+function computerPlay(){
     let toSelect = ["Rock", "Paper", "Scissors"]
     let computerSelection
     computerSelection = (toSelect[Math.floor(Math.random()*toSelect.length)]) 
@@ -10,10 +10,21 @@ function computerPlay(selection){
 }
 
 
-function roundPlay(){
-    
+function roundPlay(human, computer){
+    let humanCapitalized
+    humanCapitalized = human.charAt(0).toUpperCase() + human.slice(1).toLowerCase()
+    console.log(humanCapitalized)
+    if(humanCapitalized == "Rock" && computer == "Rock"){
+        console.log("it is a draw")
+    }
+    else if(humanCapitalized == "Rock" && computer == "Paper"){
+        console.log("Paper wins")
+    }else if(humanCapitalized == "Rock" && computer == "Scissors"){
+        console.log("Rock wins")
+    }
 }
 
 
 computerPlay()
+roundPlay(humanSelection, computerPlay)
 
