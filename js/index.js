b6a575f98ec8f0869f04d4bab83f06d5ce393a94
Josdanda <josdandar@gmail.com>
2022-07-04 21:54:34 -0700

    Rock Paper and Scissors finished

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 let humanSelection = prompt("Please select rock, paper or scissors");
-
+let computerResult 
 
 function computerPlay(){
     let toSelect = ["Rock", "Paper", "Scissors"]
     let computerSelection
     computerSelection = (toSelect[Math.floor(Math.random()*toSelect.length)]) 
     console.log(computerSelection)
+    computerResult = computerSelection
     return computerSelection
 }
 
@@ -21,10 +22,24 @@ function roundPlay(human, computer){
         console.log("Paper wins")
     }else if(humanCapitalized == "Rock" && computer == "Scissors"){
         console.log("Rock wins")
+    }else if(humanCapitalized == "Scissors" && computer == "Scissors"){
+        console.log("Its a draw")
+    }else if(humanCapitalized == "Scissors" && computer == "Paper"){
+        console.log("Scissors wins")
+    }else if(humanCapitalized == "Scissors" && computer == "Rock"){
+        console.log("Rock wins")
+    }else if(humanCapitalized == "Paper" && computer == "Paper"){
+        console.log("Its a draw")
+    }else if(humanCapitalized == "Paper" && computer == "Rock"){
+        console.log("Paper wins")
+    }else if(humanCapitalized == "Paper" && computer == "Scissors"){
+        console.log("Scissors wins")
+    }else{
+        console.warn("Choose a valid option");
     }
 }
 
 
 computerPlay()
-roundPlay(humanSelection, computerPlay)
+roundPlay(humanSelection, computerResult)
 
